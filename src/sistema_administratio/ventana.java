c13dc68f05922a279fe12903bda6ce5bf256bdb8
NatalyAlvarez07 <natalyarmy07@gmail.com>
2022-08-31 10:05:35 -0600

    creación de reporte HTML

--- src/sistema_administratio/ventana.java
+++ src/sistema_administratio/ventana.java
@@ -399,22 +399,26 @@ public class ventana extends JFrame {
         escribir.println("</head>");
         escribir.println("<body>");
         escribir.println("<h1>Listado de clientes en el sistema</h1>");
-        escribir.println("</body>");
-        escribir.println("</html>");
-        
         escribir.println("<table border = 1>");
         escribir.println("<tr>");
         escribir.println("<td>NIT</td> <td>Nombre</td> <td>Edad</td> <td>Genero</td>");
         escribir.println("</tr>");
-        
-        for(int i = 0; i<99; i++){
+                for(int i = 0; i<99; i++){
           if(clientes[i] != null){
-             //escribir.println("<tr>");
-             //escribir.println("<td>" + clientes[i].nit + "</td><td>" + clientes[i].nombre + "</td><td>" + clientes[i].edad + "</td><td>" + clientes[i].genero"</td>");
-             //escribir.println("</tr>");
+             escribir.println("<tr>");
+             escribir.println("<td>" + clientes[i].nit + "</td><td>" + clientes[i].nombre + "</td><td>" + clientes[i].edad + "</td><td>" + clientes[i].genero + "</td>");
+             escribir.println("</tr>");
           }
         }
         
+
+        escribir.println("</table");
+        
+        
+        
+        escribir.println("</body>");
+        escribir.println("</html>");
+       
         
         escribir.close(); 
         JOptionPane.showMessageDialog(null, "Reporte creado con exito, este se encuentra en la carpeta REPORTES");
